ad212cf9cddfa6fb74d49df253607706dd46ec73
Jose Martinez <deadworlds@me.com>
2019-01-16 20:29:46 -0800

    Setup interface for Shop view Created custom cell view Started implementing Product Class Setup shopViewController to display 2 Products for debug purposes.

---
+++ Shoppy/Product.swift
@@ -0,0 +1,31 @@
+//
+//  ProductStruct.swift
+//  Shoppy
+//
+//  Created by Jose Martinez on 1/16/19.
+//  Copyright © 2019 Jose Martinez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Product {
+    
+    var name: String
+    var price: Double
+    var description: String
+    var image: UIImage?
+
+    init?(name: String, price: Double, description: String, image: UIImage?){
+        
+        //initialization fails if there is no name or price is not a number.
+        if name.isEmpty || price.isNaN{
+            return nil
+        }
+        
+        self.name = name
+        self.price = price
+        self.description = description
+        self.image = image
+    }
+}
---
+++ Shoppy/ProductTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductTableViewCell.swift
+//  Shoppy
+//
+//  Created by Jose Martinez on 1/16/19.
+//  Copyright © 2019 Jose Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    
+    
+
+}
